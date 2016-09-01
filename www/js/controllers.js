4dc4d2236261d9ca4e64048dbfcf6defefd14032
junmper <jandro240@gmail.com>
2016-09-01 09:47:56 -0500

    agregue css y js para leaflet aun en construccion

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -52,13 +52,24 @@ angular.module('starter.controllers', ['ngMaterial','ngMessages','material.svgAs
   ];
 })
 
-.controller('ImageCtrl', function($scope){
+.controller('MapCtrl', function($scope){
         $scope.isOpen = false;
         $scope.demo = {
             isOpen: false,
             count: 0,
             selectedDirection: 'left'
         };
+        /*para centrar el mapa en esa longitud y latitud*/
+        angular.extend($scope, {
+          center: {
+            lat: 40.095,
+            lng: -3.823,
+            zoom: 12
+          },
+          defaults: {
+            scrollWheelZoom: false
+          }
+        });
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
