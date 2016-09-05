a5f3e3f2b27076c17ca9ce0d845ce27981b94a8f
junmper <jandro240@gmail.com>
2016-09-04 21:52:42 -0500

    se implementa la ubicacion con ngcordova

--- www/controllers/MapCrl.js
+++ www/controllers/MapCrl.js
@@ -2,6 +2,9 @@ angular.module('MapCtrl', ['leaflet-directive', 'ngMaterial','ngMessages', 'mate
 
   .controller('MapCtrl', function($scope,$cordovaGeolocation){
 
+    var lat = 40.095;
+    var long = -3.823;
+
     $scope.isOpen = false;
 
     $scope.demo = {
@@ -9,34 +12,64 @@ angular.module('MapCtrl', ['leaflet-directive', 'ngMaterial','ngMessages', 'mate
       count: 0,
       selectedDirection: 'left'}
 
+
     /* localizacion del dispositivo gps*/
-    var $lat = 40.095;
-    var $long = -3.823;
-    var posOptions = {timeout: 10000, enableHighAccuracy: false};
 
-    var coordenadas = $cordovaGeolocation.getCurrentPosition(posOptions).then(
-      function (position){
-        $lat  = position.coords.latitude
-        $long = position.coords.longitude
+    angular.element(document).ready(function () {
+      var posOptions = {timeout: 10000, enableHighAccuracy: false};
 
+      $cordovaGeolocation.getCurrentPosition(posOptions).then(
+        function (position){
+          lat  = position.coords.latitude
+          long = position.coords.longitude
 
-        console.log($lat);
-        return coordenadas;
+          var mainMarker = {
+            lat: lat,
+            lng: long,
+            focus: true,
+            message: "Tú estas aquí.",
+            draggable: true
+          };
+          console.log(lat);
 
-      }, function(err) {
-        console.log(err)
-        // error
+          angular.extend($scope, {
+            center: {
+              lat: lat,
+              lng: long,
+              zoom: 16
+            },
+            defaults: {
+              scrollWheelZoom: false
+            },
+            layers:{
+              baselayers:{
+                osm:{
+                  name: 'OpenStreetMap',
+                  url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  type: 'xyz'
+                }
+              }
+            },
+            markers:{
+              mainMarker: angular.copy(mainMarker)
+            }
+          });
 
-      });
+        }, function(err) {
+          alert('Por Favor Encienda el GPS');
+          // error
 
-      console.log($lat);
+        });
+    });
+
+      console.log(lat);
 
 
     /*para centrar el mapa en esa longitud y latitud*/
     angular.extend($scope, {
       center: {
-        lat: 40.095,
-        lng: -3.823,
+        lat: lat,
+        lng: long,
         zoom: 12
       },
       defaults: {
