fd736131854f0be51048ed5fc7773310bfb65ddd
junmper <jandro240@gmail.com>
2016-09-06 10:24:09 -0500

    se agrega el icono personalizado con sombra

--- www/controllers/MapCrl.js
+++ www/controllers/MapCrl.js
@@ -4,6 +4,25 @@ angular.module('MapCtrl', ['leaflet-directive', 'ngMaterial','ngMessages', 'mate
 
     var lat = 40.095;
     var long = -3.823;
+    /*icono para el marcador*/
+    var icons = {
+      blue: {
+        type: 'div',
+        iconSize: [15, 15],
+        className: 'blue',
+        iconAnchor:  [10, 10]
+      },
+      red: {
+        iconUrl: 'img/iconAgente.png',
+        shadowUrl: 'img/iconAgenteSombra.png',
+        iconSize:     [25, 40], // size of the icon
+        shadowSize:   [25, 15], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [22 , 60],  // the same for the shadow
+        popupAnchor:  [-10, -93] // point from which the popup should open relative to the iconAnchor
+      }
+    }
+
 
     $scope.isOpen = false;
 
@@ -23,12 +42,15 @@ angular.module('MapCtrl', ['leaflet-directive', 'ngMaterial','ngMessages', 'mate
           lat  = position.coords.latitude
           long = position.coords.longitude
 
+          /*marcador de posicion del agente*/
           var mainMarker = {
             lat: lat,
             lng: long,
+            icon: icons.red,
             focus: true,
-            message: "Tú estas aquí.",
-            draggable: true
+            message: "latitud: "+lat+" longitud: "+long,
+            draggable: true//de este se bloquea para que el marcador no se pueda mover
+
           };
 
 
@@ -37,18 +59,6 @@ angular.module('MapCtrl', ['leaflet-directive', 'ngMaterial','ngMessages', 'mate
               lat: lat,
               lng: long,
               zoom: 19
-            },
-            defaults: {
-              scrollWheelZoom: false
-            },
-            layers:{
-              baselayers:{
-                osm:{
-                  name: 'OpenStreetMap',
-                  url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  type: 'xyz'
-                }
-              }
             },
             markers:{
               mainMarker: angular.copy(mainMarker)
