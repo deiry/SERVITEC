9061dda07daf874c14a11f9589a1db7220cbd383
junmper <jandro240@gmail.com>
2016-09-10 22:11:18 -0500

    cambie el vector de atributos de las señales y arregle el formulario

--- www/controllers/FormCtrl.js
+++ www/controllers/FormCtrl.js
@@ -3,10 +3,8 @@ angular.module('FormCtrl',[])
 
   .controller('FormCtrl', function($scope){
     $scope.opcMuestra = [
-      {name: "Tablero", opc : opc},
-      {name: "Pedestal",opc : opc},
-      {name: "Anclaje", opc : opc},
-      {name: "Visibilidad", opc : opc},
+      {name: "Tablero", opc : opc, name1: "Pedestal",opc1 : opc},
+      {name: "Anclaje", opc : opc, name1: "Visibilidad", opc1 : opc},
       {name: "Acción a tomar",opc : opc}
     ]
 
