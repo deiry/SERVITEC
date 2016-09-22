4e41706b8a20c500914c8d63cb2003a67e759941
junmper <jandro240@gmail.com>
2016-09-22 10:11:01 -0500

    se cambia la estructura del controlador del mapa y se agrega el marcador de la señal

--- www/controllers/MapCrl.js
+++ www/controllers/MapCrl.js
@@ -3,69 +3,105 @@ angular.module('MapCtrl', ['leaflet-directive', 'ngMaterial','ngMessages', 'mate
   .controller('MapCtrl', function($scope,$cordovaGeolocation, LatLngMarcador){
     $scope.lat = 6.2518;
     $scope.long = -75.5636;
+    $scope.imgMap = "";
     var map;
+    var marcadorSeñal;
+    var marcador;
+    var tile;
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+    var Icon = L.icon({
+      iconUrl: 'img/iconAgente.png',
+      iconSize:     [38, 55], // size of the icon
+      iconAnchor:   [20,50], // point of the icon which will correspond to marker's location
+      popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+    });
 
-    angular.element(document).ready(function () {
+    var IconSeñales = L.icon({
+      iconUrl: 'img/iconSenalReglamentariaPare.png',
+      iconSize:     [38, 55], // size of the icon
+      iconAnchor:   [20,50], // point of the icon which will correspond to marker's location
+      popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+    });
+
+
+
+    $scope.$on('$ionicView.beforeEnter', function(){
+      console.log(LatLngMarcador);
+      $scope.imgMap = LatLngMarcador.img;
+
+
+      if(LatLngMarcador.lat != 0)
+      {
+        marcador.setLatLng([$scope.lat, $scope.long]);
+        marcadorSeñal.setLatLng([LatLngMarcador.lat, LatLngMarcador.lng]);
+        marcadorSeñal.addTo(map);
+      }
+
+
+    });
+
+    angular.element(document).ready(function ()
+    {
+      map = L.map('map'); //inicializamos el mapa
+      marcador = new L.marker([$scope.lat, $scope.long], {icon: Icon, draggable: true}); //
+      marcadorSeñal = new L.marker([LatLngMarcador.lat, LatLngMarcador.lng], {icon: IconSeñales, clickable: true, draggable: false});
+      tile = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom: 19});
 
       $scope.mostrarMapa();
+
+      /**
+       * evento cuando se hace click en el marcador de la señal
+       */
+      marcadorSeñal.on('mouseover',function(event){
+        alert('click');
+        var marker = event.target;
+        var position = marker.getLatLng();
+        console.log(position.lat);
+      });
+
+      marcador.on('dragend',function(event) {
+        var marker = event.target;
+        var position = marker.getLatLng();
+        console.log(position.lat);
+
+        LatLngMarcador.lat = position.lat;
+        LatLngMarcador.lng = position.lng;
+
+        if(kilometros($scope.lat,$scope.long,position.lat,position.lng) > 50)
+        {
+          marker.setLatLng([$scope.lat,$scope.long]);
+          LatLngMarcador.lat = $scope.lat;
+          LatLngMarcador.lng = $scope.long;
+        }
+
+
+      });
 
     });
 
     $scope.mostrarMapa = function()
     {
-      var posOptions = {timeout: 10000, enableHighAccuracy: false};
-      var Icon = L.icon({
-        iconUrl: 'img/iconAgente.png',
-        iconSize:     [38, 55], // size of the icon
-        iconAnchor:   [20,50], // point of the icon which will correspond to marker's location
-        popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
-      });
-      var marcador;
+
+
 
       /* localizacion del dispositivo gps*/
       $cordovaGeolocation.getCurrentPosition(posOptions).then(
         function (position){
           $scope.lat = position.coords.latitude;
           $scope.long=position.coords.longitude;
+          marcador.setLatLng([$scope.lat, $scope.long]);
+          map.setView([$scope.lat, $scope.long], 19);                //cramos el mapa
+                                    //asignamos la ubicacion gps al marcador
 
-
-          marcador = new L.marker([$scope.lat, $scope.long], {icon: Icon, draggable: true});
-
-          map = L.map('map').setView([$scope.lat, $scope.long], 19);
-
-
-          L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            {
-              /*attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',*/
-              maxZoom: 19
-          }).addTo(map);
+          tile.addTo(map);
           /*escala del mapa*/
           L.control.scale({position: 'bottomleft'}).addTo(map);
           /*circulo de radio de presiocion del gps*/
           L.circle([$scope.lat, $scope.long], 50).addTo(map);
 
-          marcador.bindPopup('<h4>hola</h4>',{offset: (6,0)});
-
           marcador.addTo(map);
 
 
-          marcador.on('dragend',function(event) {
-            var marker = event.target;
-            var position = marker.getLatLng();
-            console.log(position.lat);
-
-            LatLngMarcador.lat = position.lat;
-            LatLngMarcador.lng = position.lng;
-
-            if(kilometros($scope.lat,$scope.long,position.lat,position.lng) > 50)
-            {
-              marker.setLatLng([$scope.lat,$scope.long]);
-              LatLngMarcador.lat = $scope.lat;
-              LatLngMarcador.lng = $scope.long;
-            }
-
-
-          });
 
         }, function(err) {
           alert('Por Favor Encienda el GPS');
@@ -74,6 +110,7 @@ angular.module('MapCtrl', ['leaflet-directive', 'ngMaterial','ngMessages', 'mate
         });
 
     }
+
 
     $scope.centrarMapa = function(){
       map.locate({setView: true, maxZoom: 19});
