805fae5f39bb8c1b1e1d9ba7d6466d2c8bf925b4
deiry <sofia.navas96@gmail.com>
2016-11-07 00:49:36 -0500

    Vista formulario
    con el filtro de las señales segun su categorias

--- www/controllers/FormCtrl.js
+++ www/controllers/FormCtrl.js
@@ -1,87 +1,157 @@
 var opc = ["Bueno", "Regular", "Malo"];
 var opc2 = ["No Visible", "Poco visible", "Visible"];
-angular.module('FormCtrl',['ngMaterial','ngMessages', 'material.svgAssetsCache','ngMdIcons','ngCordova'])
+angular.module('FormCtrl', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngMdIcons', 'ngCordova'])
 
-  .controller('FormCtrl', function($scope, $cordovaCamera, LatLngMarcador){
+  .controller('FormCtrl', function ($scope, $cordovaCamera, LatLngMarcador) {
     $scope.opcMuestra = [
-      {name: "Tablero", opc : opc,
-        name2: "Pedestal",opc2 : opc},
-      {name: "Anclaje", opc : opc,
-         name2: "Visibilidad", opc2 : opc2}
+      {name: "Tablero", opc: opc, name1: "Pedestal", opc1: opc},
+      {name: "Anclaje", opc: opc, name1: "Visibilidad", opc1: opc2},
+      {name: "Acción a tomar", opc: opc}
     ];
-        $scope.categoria =[
-            {
-              name: "Señal Reglamentaria",
-              icon:"hola"
-            },
-            {
-                name: "Señal Preventiva",
-                icon:"hola"
-            },
-            {
-                name: "Señal Informativa",
-                icon:"hola"
-            }];
+    $scope.categoriaSenales = [
+      {
+        id: 1,
+        name: "Señal Reglamentaria",
+        icon: "hola"
+      },
+      {
+        id: 2,
+        name: "Señal Preventiva",
+        icon: "hola"
+      },
+      {
+        id: 3,
+        name: "Señal Informativa",
+        icon: "hola"
+      }];
 
-        $scope.SR= [{
-            "01": {
-                "nombre": "01: Pare",
-                    "icon": "hola"
-            },
-            "02": {
-                "nombre": "02: Ceda el paso",
-                    "icon": "hola"
-            },
-            "03": {
-                "nombre": "03: Siga de frente",
-                    "icon": "hola"
-            },
-            "04": {
-                "nombre": "04: No pase",
-                    "icon": "hola"
-            },
-            "05": {
-                "nombre": "05: Giro a la izq. solamente",
-                    "icon": "hola"
-            },
-            "06": {
-                "nombre": "06: Prohibido girar a la izq.",
-                    "icon": "hola"
-            }
-        }]
+    $scope.categoriaFiltro = '';
 
-        $scope.SP= [{
-            "01": {
-                "nombre": "01: Curva cerrada a la izq.",
-                "icon": "hola"
-            },
-            "02": {
-                "nombre": "02: Curva cerrada a la der.",
-                "icon": "hola"
-            },
-            "03": {
-                "nombre": "03: Curva pronunciada a la izq.",
-                "icon": "hola"
-            },
-            "04": {
-                "nombre": "04: Curva pronunciada a la der.",
-                "icon": "hola"
-            },
-            "05": {
-                "nombre": "05: Curva y contracurva cerrada primera a la izq.",
-                "icon": "hola"
-            },
-            "06": {
-                "nombre": "06: Curva y contracurva cerrada primera a la der.",
-                "icon": "hola"
-            }
-        }]
+    $scope.senales = [
+      {
+        id: 1,
+        nombre: "01: Pare",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 1
+      },
+      {
+        id: 2,
+        nombre: "02: Ceda el paso",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 1
+      },
+      {
+        id: 3,
+        nombre: "03: Siga de frente",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 1
+      },
+      {
+        id: 4,
+        nombre: "04: No pase",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 1
+      },
+      {
+        id: 5,
+        nombre: "05: Giro a la izq. solamente",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 1
+      },
+      {
+        id: 6,
+        nombre: "06: Prohibido girar a la izq.",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 1
+      },
+      {
+        id: 1,
+        nombre: "01: Curva cerrada a la izq.",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 2
+      },
+      {
+        id: 2,
+        nombre: "02: Curva cerrada a la der.",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 2
 
+      },
+      {
+        id: 3,
+        nombre: "03: Curva pronunciada a la izq.",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 2
+
+      },
+      {
+        id: 4,
+        nombre: "04: Curva pronunciada a la der.",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 2
+
+      },
+      {
+        id: 5,
+        nombre: "05: Curva y contracurva cerrada primera a la izq.",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 2
+
+      },
+      {
+        id: 6,
+        nombre: "06: Curva y contracurva cerrada primera a la der.",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 2
+      },
+      {
+        id: 1,
+        nombre: "01: Ruta nacional",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 3
+      },
+      {
+        id: 2,
+        nombre: "01A: Ruta departamental",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 3
+      },
+      {
+        id: 3,
+        nombre: "02: Ruta panamericana",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 3
+      },
+      {
+        id: 4,
+        nombre: "03: Ruta marginal de selva",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 3
+      },
+      {
+        id: 5,
+        nombre: "04: Sitio de parqueo",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 3
+      },
+      {
+        id: 6,
+        nombre: "07A: :Zona especial de parqueo",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 3
+      },
+      {
+        id: 7,
+        nombre: "08: Paradero de buses",
+        img: "img/iconSenalReglamentaria.PNG",
+        categoria: 3
+      }
+    ]
     angular.element(document).ready(function () {
-     $scope.tomarFoto();
+      $scope.tomarFoto();
     });
 
-    $scope.tomarFoto = function(){
+    $scope.tomarFoto = function () {
       var options = {
         quality: 80,
         destinationType: Camera.DestinationType.DATA_URL,
@@ -103,5 +173,12 @@ angular.module('FormCtrl',['ngMaterial','ngMessages', 'material.svgAssetsCache',
         alert(err);
       });
     };
+
+    $scope.seleccionCategoria = function(id)
+    {
+      $('#contenedorTipoSenal').slideToggle(200);
+      $scope.categoriaFiltro = id;
+      console.log(id);
+    }
 
   });
